de026630d9cb9c58facbaf48940d81371da23cfd
jayspringate <jay.springate@gmail.com>
2015-06-15 12:11:39 -0700

    adjust for mongo

--- tests/freezr_api_test.js
+++ tests/freezr_api_test.js
@@ -1,25 +1,24 @@
 'use strict';
 
-require('../server');
-
+var server = require('../server');
+process.env.MONGOLAB_URI = 'mongodb://localhost/players_development';
+var mongoose = require('mongoose');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 var expect = chai.expect;
 var User = require('../models/User'); //double-check naming
+var bodyparser = require('body-parser');
+var eat = require('eat');
 
 describe('api users routes', function() {
   after(function(done) {
-    User.destroy({
-      where: {
-        username: testuser
-      }
-    }).then(function() {
+    mongoose.connection.db.dropDatabase(function() {
       done();
     });
   });
 
-  var newEat;
+  var newToken;
 
   it('should be able to create a test user', function(done) {
     chai.request('localhost:3000')
@@ -28,7 +27,7 @@ describe('api users routes', function() {
              email: 'testuser@test.com',
              password: 'test'})
       .end(function(err, res) {
-        newEat = res.body.token;
+        newToken = res.body.token;
         expect(err).to.eql(null);
         expect(res.body.username).to.eql('testuser');
         expect(res.body.email).to.eql('testuser@test.com');
@@ -44,7 +43,7 @@ describe('api users routes', function() {
       .get('/api/sign_in')
       .set('email', 'testuser@test.com')
       .set('password', 'test')
-      .set('token', newEat)
+      .set('token', newToken)
       .end(function(err, res) {
         expect(err).to.eql(null);
         //possibly add more expects here
