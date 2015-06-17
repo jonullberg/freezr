61b295a149073e6e0b8b68fab74adf4346bc1c33
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-06-17 14:24:33 -0700

    started some tests

---
+++ test/karma-test/food_data_test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('../../app/js/client');
+
+require('angular-mocks');
+
+describe('Food Data Service', function() {
+  var
+
+  beforeEach(angular.mock.module('freezrApp'));
+
+  beforeEach(angular.mock.inject())
+
+  it('Should be able to create a new service', function() {
+    var food_data =
+  });
+});
