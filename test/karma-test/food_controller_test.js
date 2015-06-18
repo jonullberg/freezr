c17e115458465f64861d98dc0454c34f6979e811
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-06-18 14:49:01 -0700

    added tests for food_controller

--- test/karma-test/food_controller_test.js
+++ test/karma-test/food_controller_test.js
@@ -7,7 +7,21 @@ describe('food controller', function() {
   //this is the controller constructor
   var $ControllerConstructor;
   var $scope;
+  var $location;
   var foodData;
+  var fakeData = {
+    _id: 'myId'
+  };
+  var fakeStoreData = [
+    {
+      _id: 'myId',
+      name: 'myName'
+    },
+    {
+      _id: 'fakeId',
+      name: 'fakeName'
+    }
+  ];
 
   beforeEach(angular.mock.module('freezrApp'));
 
@@ -26,16 +40,34 @@ describe('food controller', function() {
     expect(Array.isArray($scope.allItems)).toBe(true);
     expect(Array.isArray($scope.displayedItems)).toBe(true);
     expect(Array.isArray($scope.errors)).toBe(true);
+    expect(typeof $scope.saveSingleFood).toBe('function');
+    expect(typeof $scope.getAll).toBe('function');
+    expect(typeof $scope.addDaysProperty).toBe('function');
+    expect(typeof $scope.getDisplayedItems).toBe('function');
+    expect(typeof $scope.createNewItem).toBe('function');
+    expect(typeof $scope.removeItem).toBe('function');
+    expect(typeof $scope.clearErrors).toBe('function');
+    expect(typeof $scope.populateImages).toBe('function');
+    expect(typeof $scope.editItem).toBe('function');
+    expect(typeof $scope.cancelEditing).toBe('function');
+
   });
-  // describe('Food controller functions', function() {
-  //   beforeEach(angular.mock.inject(function(_foodData_) {
-  //     foodData = _foodData_;
-  //     this.foodController = $ControllerConstructor('foodController', {$scope: $scope});
-  //   }));
+  describe('Food controller functions', function() {
+    beforeEach(angular.mock.inject(function(_$location_, _foodData_) {
+      $location = _$location_;
+      foodData = _foodData_;
+      this.foodController = $ControllerConstructor('foodController', {$scope: $scope});
+      foodData.storeData(fakeStoreData);
+    }));
 
-  //   describe('Saving Single Food function', function() {
+    describe('Saving Single Food function', function() {
 
-  //     it('Should store a single food on foodData.singleFood', function() {
-  //       console.log($scope.saveSingleFood);
-  //     });
+      it('Should store a single food on foodData.singleFood', function() {
+        $scope.saveSingleFood(fakeData);
+        expect(foodData.singleFood[0].name).toBe('myName');
+        expect($location.path()).toBe('/item');
+      });
+    });
+
+  });
 });
